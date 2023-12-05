d4a3db58121ce9d8ac1bfb4c8dc46005337f5248
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-12-05 20:54:29 -0300

    Separar os templates de home e recipe

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={'name': 'Marshal'})
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Marshal',
+    })
